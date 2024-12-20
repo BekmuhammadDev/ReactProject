55c8ed9745cdb100dc04e0ba13e9cee6307b9485
bekmuhammad <aripovbekmuhammad2@gmail.com>
2024-12-20 15:38:03 +0500

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './components/Home'
-import Footer from './components/Footer';
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import { ClipLoader } from "react-spinners";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <ClipLoader color="#3498db" size={50} />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <Navbar />
+      <Home />
+      <Footer />
     </div>
   );
 };
